466fd1fcab49706ff8823feac6f0e68b822a1046
wajihov <wajihov@hotmail.fr>
2018-11-24 18:36:13 +0100

    insertion des données dans la BD et afficher ds le tableau de clients

---
+++ src/main/java/com/access/hr/controllers/ClientController.java
@@ -0,0 +1,30 @@
+package com.access.hr.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.access.hr.entities.Client;
+import com.access.hr.services.IClientService;
+
+@Controller
+@RequestMapping(value = "/client")
+public class ClientController {
+
+	@Autowired
+	private IClientService clientService;
+
+	@RequestMapping(value = "/")
+	public String client(Model model) {
+		List<Client> clients = clientService.selectAll();
+		if (clients == null) {
+			clients = new ArrayList<Client>();
+		}
+		model.addAttribute("clients", clients);
+		return "client/client";
+	}
+}
